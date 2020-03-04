2f8749dba2732ee0a7becfbff96170f7d5a9a17d
Nikhil <nikhil.rajput@myntra.com>
2020-03-04 15:00:25 +0530

    Sum-Fib Solution

--- js-exercises/sum-fibs/sumFibs.js
+++ js-exercises/sum-fibs/sumFibs.js
@@ -1,7 +1,40 @@
-function sumFibs() {
+const fibonacciNumberMap = new Map();
+fibonacciNumberMap.set(0, 1);
+fibonacciNumberMap.set(1, 1);
 
+function getFibonacciNumber(position) {
+  const valueInFibonacciNumberMap = fibonacciNumberMap[position];
+  if (
+    valueInFibonacciNumberMap !== undefined
+    && valueInFibonacciNumberMap !== null
+  ) {
+    return valueInFibonacciNumberMap;
+  }
+  let positionToFind = 2;
+  while (positionToFind <= position) {
+    fibonacciNumberMap.set(
+      positionToFind,
+      fibonacciNumberMap.get(positionToFind - 1)
+        + fibonacciNumberMap.get(positionToFind - 2),
+    );
+    positionToFind += 1;
+  }
+
+  return fibonacciNumberMap.get(position);
 }
 
-export {
-  sumFibs,
-};
+function sumFibs(number) {
+  let sumOfOddFibs = 0;
+  let fibonacciPositon = 0;
+  let fibonacciNumber = getFibonacciNumber(fibonacciPositon);
+
+  while (fibonacciNumber <= number) {
+    if (fibonacciNumber % 2 !== 0) {
+      sumOfOddFibs += fibonacciNumber;
+    }
+    fibonacciPositon += 1;
+    fibonacciNumber = getFibonacciNumber(fibonacciPositon);
+  }
+  return sumOfOddFibs;
+}
+export { sumFibs };
