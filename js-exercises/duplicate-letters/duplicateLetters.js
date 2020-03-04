b46faa9e5a8b3ea960f945725731e8b4306cb0bd
Nikhil <nikhil.rajput@myntra.com>
2020-03-04 13:46:41 +0530

    Duplicate-Letters Solution

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,8 +1,27 @@
-
-function duplicateLetters(...args) {
-  return args;
+function buildCharacterFrequencyMap(string) {
+  const characterFrequencyMap = new Map();
+  for (const character of string) {
+    characterFrequencyMap.set(character, (characterFrequencyMap.get(character)
+      ? characterFrequencyMap.get(character) + 1
+      : 1));
+  }
+  return characterFrequencyMap;
 }
 
-export {
-  duplicateLetters,
-};
+function getMaxCharacterCount(characterFrequencyMap) {
+  let maxCharacterCount = 0;
+  for (const individualCharacterCount of characterFrequencyMap.values()) {
+    if (maxCharacterCount < individualCharacterCount) {
+      maxCharacterCount = individualCharacterCount;
+    }
+  }
+  return maxCharacterCount;
+}
+
+function duplicateLetters(...string) {
+  const characterFrequencyMap = buildCharacterFrequencyMap(...string);
+  const maxCharacterCount = getMaxCharacterCount(characterFrequencyMap);
+  return maxCharacterCount > 1 ? maxCharacterCount : false;
+}
+
+export { duplicateLetters };
