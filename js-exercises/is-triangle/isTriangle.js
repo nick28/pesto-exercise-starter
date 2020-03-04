4bcf6bd0d6c221d01426e8ed33ff13ef0f402d63
Nikhil <nikhil.rajput@myntra.com>
2020-03-04 11:33:31 +0530

    is-Triangle Solution using Triangle Inequality Theorem

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,5 +1,10 @@
-function isTriangle() {
+function isTriangle(sideLength1, sideLength2, sideLength3) {
+// Verify Triangle InEquality Theorem - Sum of Any two sides length 
+// should be greater than the remaining one
+  const largerSideLength = Math.max(sideLength1, sideLength2, sideLength3);
 
+  // In short Sum of two smaller sides should be greater than the larger side
+  return (sideLength1 + sideLength2 + sideLength3 - largerSideLength) > largerSideLength;
 }
 
 export {
