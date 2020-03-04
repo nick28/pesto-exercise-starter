ebe301d51a905f269e7041d9e518e94d9bcdfae6
Nikhil <nikhil.rajput@myntra.com>
2020-03-04 17:47:57 +0530

    Math-fns Solution Initial Commit

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,16 @@
+// Don't change the export values.
+// You can use the function in the Math module
+
+function sqrt(number) {
+  return Math.sqrt(number);
+}
+
+function power(baseValue, specifiedPower) {
+  return baseValue ** specifiedPower;
+}
+
+function round(number) {
+  return Math.round(number);
+}
+
+export { sqrt, power, round };
