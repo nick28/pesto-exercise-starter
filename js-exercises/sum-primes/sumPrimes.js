23b236920e8aa2674a62e72b4127e01e3aea5fff
Nikhil <nikhil.rajput@myntra.com>
2020-03-04 15:27:31 +0530

    Sum-Prime Solution

--- js-exercises/sum-primes/sumPrimes.js
+++ js-exercises/sum-primes/sumPrimes.js
@@ -1,7 +1,19 @@
-function sumPrimes() {
+const isPrime = num => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) if (num % i === 0) return false;
+  return num > 1;
+};
 
+function sumPrimes(number) {
+  let sumOfPrimeNumbers = 0;
+  let wholeNumber = 2;
+
+  while (wholeNumber <= number) {
+    if (isPrime(wholeNumber)) {
+      sumOfPrimeNumbers += wholeNumber;
+    }
+    wholeNumber += 1;
+  }
+  return sumOfPrimeNumbers;
 }
 
-export {
-  sumPrimes,
-};
+export { sumPrimes };
